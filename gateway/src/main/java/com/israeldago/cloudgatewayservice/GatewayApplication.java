a7f439be029e3aa752ecd9d1a43bb2559b118b7a
Israel Dago <dagoisrael@gmail.com>
2021-11-13 19:55:40 +0200

    Externalized config for all modules

--- gateway/src/main/java/com/israeldago/cloudgatewayservice/GatewayApplication.java
+++ gateway/src/main/java/com/israeldago/cloudgatewayservice/GatewayApplication.java
@@ -8,15 +8,14 @@ import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CloudGatewayServiceApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudGatewayServiceApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient rdc,
-                                                                                DiscoveryLocatorProperties dlp) {
+    @Bean("discoveryClientRouteDefinitionLocator")
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
         return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 }
